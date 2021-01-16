1cfe9ea86ea2d89895fed1117b044d422848e678
Michael Chen <atlasneiko@gmail.com>
2021-01-16 11:42:44 -0800

    setup-2

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require("body-parser");
 const app = express();
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
+const passport = require("passport");
+const path = require("path");
+const users = require("./routes/api/users")
 
+//* connect to mongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+//* setup passport
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
-app.get("/", (req, res) => res.send("Hello World! >///w///<"))
 
 const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Hello World! >///w///<"))
+app.use("/api/users", users)
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
