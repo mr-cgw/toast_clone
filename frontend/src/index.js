6a0652056ed59405cc5928d68df44f6772d6d0e6
Michael Chen <atlasneiko@gmail.com>
2021-01-16 14:04:00 -0800

    setup frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
+
+    store = configureStore(preloadedState);
+
+    const currentTime = Date.now() / 1000;
+
+    if (decodedUser.exp < currentTime) {
+      store.dispatch(logout());
+      window.location.href = '/login';
+    }
+  } else {
+    store = configureStore({});
+  }
+  const root = document.getElementById('root');
+  window.getState = store.getState;
+  ReactDOM.render(<Root store={store} />, root);
+});
