c9c4e80bbb04f4ef8439e1fa341148d1676ffce2
Michael Chen <atlasneiko@gmail.com>
2021-01-17 12:17:31 -0800

    remove password2

--- validation/register.js
+++ validation/register.js
@@ -7,7 +7,6 @@ module.exports = function validateRegisterInput(data) {
   data.handle = validText(data.username) ? data.username : '';
   data.email = validText(data.email) ? data.email : '';
   data.password = validText(data.password) ? data.password : '';
-  data.password2 = validText(data.password2) ? data.password2 : '';
 
   if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
     errors.handle = 'Username must be between 2 and 30 chars';
@@ -25,17 +24,13 @@ module.exports = function validateRegisterInput(data) {
     errors.email = 'Email is invalid';
   }
 
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password is required';
-  }
+
 
   if (!Validator.isLength(data.password, { min: 2, max: 30 })) {
     errors.password = 'Password must be between 2 and 30 chars';
   }
 
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Password must match';
-  }
+
 
   return {
     errors,
