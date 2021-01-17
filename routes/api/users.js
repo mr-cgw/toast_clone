0c2afa521dca9cf3c2f4f83bb9b2f7c6b2d23fb4
Michael Chen <atlasneiko@gmail.com>
2021-01-17 11:12:41 -0800

    make update user

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,18 +10,6 @@ const validateLoginInput = require('../../validation/login');
 
 router.get('/test', (req, res) => res.json({ msg: 'This is the users route' }));
 
-router.get(
-  '/current',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      handle: req.user.handle,
-      email: req.user.email,
-    });
-  }
-);
-
 router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
@@ -36,8 +24,10 @@ router.post('/register', (req, res) => {
         .json({ email: 'A user is already registered with that email' });
     } else {
       const newUser = new User({
-        email: req.body.email,
         password: req.body.password,
+        email: req.body.email,
+        username: req.body.username,
+        avatarUrl: req.body.avatarUrl
       });
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -45,7 +35,21 @@ router.post('/register', (req, res) => {
           newUser.password = hash;
           newUser
             .save()
-            .then((user) => { res.json(user) })
+            .then((user) => {
+              const payload = { id: user.id, email: user.email };
+              jwt.sign(
+                payload,
+                keys.secretOrKey,
+                { expiresIn: 24 * 3600 },
+                (err, token) => {
+                  res.json({
+                    success: true,
+                    token: 'Bearer ' + token,
+                    user: user
+                  });
+                }
+              );
+            })
             .catch((err) => console.log(err));
         });
       });
@@ -81,6 +85,7 @@ router.post('/signin', (req, res) => {
             res.json({
               success: true,
               token: 'Bearer ' + token,
+              user: user
             });
           }
         );
@@ -92,23 +97,44 @@ router.post('/signin', (req, res) => {
   });
 });
 //show other user profile
-router.get('/:user_id', (req, res) => {
-  User.findById(req.params.user_id)
+router.get('/:userId', (req, res) => {
+  User.findById(req.params.userId)
     .then((user) => res.json(user))
     .catch((err) => res.status(400).json(err));
 });
-//update user tags
+
+//update user
 router.patch(
-  '/update',
+  '/update/:userId',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
-    User.findOneAndUpdate(
-      { id: req.body.user_id },
-      { tag: req.body.tag },
-      { new: true }
-    )
-      .then((user) => res.json(user))
-      .catch((err) => res.status(400).json(err));
+    const newUser = {
+      password: req.body.password,
+      email: req.body.email,
+      username: req.body.username,
+      avatarUrl: req.body.avatarUrl
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash
+
+        User.findOneAndUpdate(
+          { _id: req.params.userId },
+          newUser,
+          { new: true }
+        )
+          .then((user) => {
+            res.json(user)
+          })
+          .catch((err) => res.status(400).json(err));
+      });
+    });
+
+
+
+
   }
 );
 module.exports = router;
