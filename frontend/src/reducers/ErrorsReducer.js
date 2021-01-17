4549b286950ab6730972102698848cdb828de444
Yizhe Wang <yizhe87@gmail.com>
2021-01-17 14:53:46 -0800

    merge with master

---
+++ frontend/src/reducers/ErrorsReducer.js
@@ -0,0 +1,10 @@
+// const _initState = {};
+// export default (oldState = {}, action) => {
+//   Object.freeze(oldState);
+//   switch (action.type) {
+//     case CONST:
+//       return STATE;
+//     default:
+//       return oldState;
+//   }
+// };
