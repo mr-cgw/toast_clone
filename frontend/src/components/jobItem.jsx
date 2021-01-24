131c3dcf32790c6bdb93794f241f9ce00abacaeb
Michael Chen <atlasneiko@gmail.com>
2021-01-23 16:29:53 -0800

    make app post from

--- frontend/src/components/jobItem.jsx
+++ frontend/src/components/jobItem.jsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactTimeAgo from 'react-time-ago';
-import TimeAgo from 'javascript-time-ago';
+import React from "react";
+import ReactTimeAgo from "react-time-ago";
+import TimeAgo from "javascript-time-ago";
+import { Link } from "react-router-dom";
 
-import en from 'javascript-time-ago/locale/en';
+import en from "javascript-time-ago/locale/en";
 
 TimeAgo.addDefaultLocale(en);
 // English.
 function JobItem({ job, location, position }) {
-  if (job.location.includes(location) || location === 'All') {
-    if (job.category.includes(position) || position === 'All-Pos') {
-      return (
-        <div className="job-item">
-          <span className="job-company">{job.company}</span>
-          <img src={job.companyLogo} />
-          <span className="job-position">{job.position}</span>
-          <span className="job-location">{job.location}</span>
-          <span className="job-time">
-            <ReactTimeAgo date={job.time} locale="en-US" />
-          </span>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  } else {
-    return <div></div>;
-  }
+	if (job.location.includes(location) || location === "All") {
+		if (job.category.includes(position) || position === "All-Pos") {
+			return (
+				<Link to={{ pathname: "/newApplication", data: job }}>
+					<div className="job-item">
+						<span className="job-company">{job.company}</span>
+						<img src={job.companyLogo} />
+						<span className="job-position">{job.position}</span>
+						<span className="job-location">{job.location}</span>
+						<span className="job-time">
+							<ReactTimeAgo date={job.time} locale="en-US" />
+						</span>
+					</div>
+				</Link>
+			);
+		} else {
+			return <div></div>;
+		}
+	} else {
+		return <div></div>;
+	}
 }
 
 export default JobItem;
