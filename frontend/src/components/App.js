8644480bada9957116b982d3154cca18fa61d7c5
Yizhe Wang <yizhe87@gmail.com>
2021-01-29 17:09:49 -0800

    fixed protected route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,9 +11,9 @@ import Profolio from './profolio/profolio_container';
 import ApplicationList from './ApplicationList';
 import ApplicationDetailItem from './ApplicationDetailItem';
 import Footer from './Footer';
-import Charts from './charts/allChartsContainer'
-import ApplicationPostForm from './appForms/appPostForm'
-import ApplicationEditForm from './appForms/appEditForm'
+import Charts from './charts/allChartsContainer';
+import ApplicationPostForm from './appForms/appPostForm';
+import ApplicationEditForm from './appForms/appEditForm';
 
 function App() {
   const loggedIn = false;
@@ -25,12 +25,20 @@ function App() {
         <AuthRoute exact path="/signin" component={Signin} />
         <AuthRoute exact path="/signup" component={Signup} />
         <ProtectedRoute exact path="/edit" component={Profolio} />
-        <ProtectedRoute exact path="/newApplication" component={ApplicationPostForm} />
-        <ProtectedRoute exact path="/editApplication/:applicationId" component={ApplicationEditForm} />
+        <ProtectedRoute
+          exact
+          path="/newApplication"
+          component={ApplicationPostForm}
+        />
+        <ProtectedRoute
+          exact
+          path="/editApplication/:applicationId"
+          component={ApplicationEditForm}
+        />
         <Route exact path="/" component={Landing} />
         {/* <ProtectedRoute exact path='/edit' component={EditForm} /> */}
-        <Route exact path="/item" component={ApplicationDetailItem} />
-        <Route exact path="/list" component={ApplicationList} />
+        <Protected exact path="/item" component={ApplicationDetailItem} />
+        <Protected exact path="/list" component={ApplicationList} />
       </Switch>
       {location.pathname === '/signin' || location.pathname === '/signup' ? (
         <div></div>
