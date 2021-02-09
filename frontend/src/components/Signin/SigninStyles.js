952f4ba30d74feaf1ac5c45990da62c78b58ca62
Yizhe Wang <yizhe87@gmail.com>
2021-02-08 22:06:03 -0800

    fixed navhight

--- frontend/src/components/Signin/SigninStyles.js
+++ frontend/src/components/Signin/SigninStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100vw',
-    height: '100vh',
+    height: '70vh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
